41ddff0374e2db6864e75d9e22dfd3cb35772dbc
malaravan236@gmail.com <malaravan236@gmail.com>
2026-09-11 20:19:04 +0530

    Configure backend database

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -9,6 +9,10 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 from pathlib import Path
 
@@ -20,7 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$t%!&s!vm6m_-aazgb6-bf)zxx+#sv3fzsxemmieh%q-@2^9m-'
+
+# SECRET_KEY = 'django-insecure-$t%!&s!vm6m_-aazgb6-bf)zxx+#sv3fzsxemmieh%q-@2^9m-'
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -63,12 +70,14 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://globalglimpse-lake.vercel.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
 
 CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:5173",
+    "https://globalglimpse-lake.vercel.app",
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -94,14 +103,19 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'news_aggregator_db',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'NAME': os.environ.get('DB_NAME', 'defaultdb'),
+        'USER': os.environ.get('DB_USER', 'avnadmin'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT', '21144'),
+        'OPTIONS': {
+            'ssl': {}
+        }
     }
 }
 
