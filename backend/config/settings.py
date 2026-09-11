e7284981712aefd5a4eb1e8f38c1cdbc99bb505a
malaravan236@gmail.com <malaravan236@gmail.com>
2026-09-11 17:54:40 +0530

    Prepare Django backend

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -23,10 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$t%!&s!vm6m_-aazgb6-bf)zxx+#sv3fzsxemmieh%q-@2^9m-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".vercel.app",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 CORS_ALLOW_CREDENTIALS = True
